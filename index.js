489158a9a0425d22d72d475b6d293b5bc27a48da
saytkhanov <saythanov2014@mail.ru>
2021-05-17 01:12:34 +0300

    add

--- index.js
+++ index.js
@@ -4,13 +4,34 @@ window.addEventListener('DOMContentLoaded', () => {
       return res.json();
     })
     .then((data) => {
+    const count = document.querySelector('#count');
+    count.textContent = data.number;
+    for(let i = 0; i < data.people.length; i++) {
+      const table = document.querySelector('#table')
+      const tr = document.createElement('tr');
+      const num = document.createElement('td')
+      const place = document.createElement('td')
+      const names = document.createElement('td');
 
-      // ТВОЕ РЕШЕНИЕ
+      tr.append(num, place, names);
+
+      place.textContent = data.people[i].craft;
+
+      names.textContent = data.people[i].name;
+
+      num.textContent = i + 1
+
+      table.append(tr)
+
+    }
 
     })
     .catch(() => {
-      
-      // ОБРАБОТКА ОШИБОК
+      count.textContent = 'Неизвестно'
+      const div = document.createElement('div');
+      div.classList.add('error');
+      div.textContent = 'Error'
+      table.replaceWith(div)
 
     })
 });
